70f6e31eed99273c760694bbee1b6c1aec99de37
Luis Enrique <luisenriquecgongora@gmail.com>
2019-12-29 14:47:47 +0100

    Added cached

--- sw.js
+++ sw.js
@@ -1,5 +1,33 @@
+const staticCacheName = 'sitestatic';
+const assets = [
+  '',
+  'index.html',
+  'js/app.js',
+  'js/ui.js',
+  'js/materialize.min.js',
+  'css/styles.css',
+  'css/materialize.min.css',
+  'img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
 self.addEventListener('install',(evt)=>{
-  console.log('service worker has been installed');
+  evt.waitUntil(
+    caches.open(staticCacheName)
+    .then((cache)=>{
+      console.log('caching shell assets');
+      cache.addAll(assets)
+      .then((cached)=>{
+        console.log(cached);
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  );
 })
 
 self.addEventListener('activate',(evt)=>{
@@ -7,5 +35,9 @@ self.addEventListener('activate',(evt)=>{
 })
 
 self.addEventListener('fetch',(evt)=>{
-  console.log("fetch event", evt);
-})
+  evt.respondWith(
+    caches.match(evt.request).then((cacheRes)=>{
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
